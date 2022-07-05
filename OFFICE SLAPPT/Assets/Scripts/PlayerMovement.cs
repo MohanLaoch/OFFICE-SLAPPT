6a0f44d13f34e7ea210b041281da247d4a9d5d6d
James Mohan <c20305011@mytudublin.ie>
2022-07-05 15:23:55 +0100

    Player can Jump

--- OFFICE SLAPPT/Assets/Scripts/PlayerMovement.cs
+++ OFFICE SLAPPT/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,18 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpSpeed;
 
+    [SerializeField] private LayerMask ground;
+
     private PlayerControls playerControls;
+
+    private Rigidbody2D rigidBody;
+    private Collider2D collider;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
+        rigidBody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
     }
 
     private void OnEnable()
@@ -25,17 +32,35 @@ public class PlayerMovement : MonoBehaviour
         playerControls.Disable();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        playerControls.MovementMap.Jump.performed += _ => Jump();
     }
 
-    // Update is called once per frame
+    private void Jump()
+    {
+        if (isGrounded())
+        {
+            rigidBody.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        Vector2 topLeftPoint = transform.position;
+        topLeftPoint.x -= collider.bounds.extents.x;
+        topLeftPoint.y += collider.bounds.extents.y;
+
+        Vector2 bottomRightPoint = transform.position;
+        bottomRightPoint.x += collider.bounds.extents.x;
+        bottomRightPoint.y -= collider.bounds.extents.y;
+
+        return Physics2D.OverlapArea(topLeftPoint, bottomRightPoint, ground);
+    }
+
     void Update()
     {
         // Reading Movement Value
-
         float movementInput = playerControls.MovementMap.Move.ReadValue<float>();
 
         // Moving the Player
